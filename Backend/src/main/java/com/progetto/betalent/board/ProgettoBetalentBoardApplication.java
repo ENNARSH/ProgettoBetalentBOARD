a728ff2e1e1deae958e29ef61b55471da5131403
Ennarsh <gennaro.liguori.sh@gmail.com>
2023-10-30 15:52:41 +0100

    firstCommit

---
+++ Backend/src/main/java/com/progetto/betalent/board/ProgettoBetalentBoardApplication.java
@@ -0,0 +1,13 @@
+package com.progetto.betalent.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBetalentBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBetalentBoardApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/progetto/betalent/board/ProgettoBetalentBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto.betalent.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoBetalentBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
