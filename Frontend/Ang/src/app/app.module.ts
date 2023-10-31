65d20c46a3c719ed6625274e7e3ea033afb9da55
Jonathan <jonathanlmb97@gmail.com>
2023-10-31 21:22:19 +0100

    main page add

--- Frontend/Ang/src/app/app.module.ts
+++ Frontend/Ang/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+//COMPONENT
 import { AppComponent } from './app.component';
 import { HomeComponent } from './homeC/home/home.component';
+import { NavBarComponent } from './navBar/nav-bar/nav-bar.component';
+//MATERIAL
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FooterComponent } from './footer/footer/footer.component';
+
+
+//
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
