0527ca609c515ebd09a380fd333344ba1c43c92e
Jonathan <jonathanlmb97@gmail.com>
2023-10-31 16:09:53 +0100

    add mvc and restController

--- Backend/src/main/java/com/progetto/betalent/board/entities/Prodotto.java
+++ Backend/src/main/java/com/progetto/betalent/board/entities/Prodotto.java
@@ -4,11 +4,20 @@ package com.progetto.betalent.board.entities;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
 @Entity  //Questa classe corrisponde a una tabella
 @Table(name="prodotti") //corrisponde al nome della tabella (entità)
 public class Prodotto {
-	
+
 	@Id  // Obbligatorio
 	@GeneratedValue(strategy = GenerationType.IDENTITY)  //ID incrementale su tutta la tabella
 	@Column(name = "id")  //corrisponde al nome della colonna (attributo)
@@ -24,7 +33,7 @@ public class Prodotto {
 	private String codici;
 
 	@Column(name = "auto_compatibile") // qashqai mk1
-    private String autoCompatibile;
+	private String autoCompatibile;
 
 	@Column(name = "descrizione") // scassato
 	private String descrizione;
@@ -33,69 +42,5 @@ public class Prodotto {
 	private Long prezzo;
 
 
-
-	public Prodotto() {
-		super();
-	}
-	
-    // Getters e Setters
-
-
-	public Long getIdProdotto() {
-		return idProdotto;
-	}
-
-	public void setIdProdotto(Long idProdotto) {
-		this.idProdotto = idProdotto;
-	}
-
-	public String getCodProdotto() {
-		return codProdotto;
-	}
-
-	public void setCodProdotto(String codProdotto) {
-		this.codProdotto = codProdotto;
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
-	public String getCodici() {
-		return codici;
-	}
-
-	public void setCodici(String codici) {
-		this.codici = codici;
-	}
-
-	public String getAutoCompatibile() {
-		return autoCompatibile;
-	}
-
-	public void setAutoCompatibile(String autoCompatibile) {
-		this.autoCompatibile = autoCompatibile;
-	}
-
-	public String getDescrizione() {
-		return descrizione;
-	}
-
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-
-	public Long getPrezzo() {
-		return prezzo;
-	}
-
-	public void setPrezzo(Long prezzo) {
-		this.prezzo = prezzo;
-	}
 }
-	
 
