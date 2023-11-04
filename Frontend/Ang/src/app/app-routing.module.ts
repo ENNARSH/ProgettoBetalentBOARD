1f517dd0bb764bd1bd0b59ec4491bb5734c7f9ba
Jonathan <jonathanlmb97@gmail.com>
2023-11-04 13:40:31 +0100

    del and get add

--- Frontend/Ang/src/app/app-routing.module.ts
+++ Frontend/Ang/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './homeC/home/home.component';
+import { EditComponent } from './addEdit/edit/edit.component';
+import { AddComponent } from './addEdit/add/add.component';
 
 const routes: Routes = [
+  {path:"" , component: HomeComponent,},
   {path:"home" , component: HomeComponent,},
+  {path:"edit/:codProdotto" , component: EditComponent,},
+  {path:"edit" , component: EditComponent,},
+  {path:"add" , component: AddComponent,},
 ];
 
 @NgModule({
